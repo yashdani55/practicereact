b495094b2fee334cdf283dee554468bf9263fcf6
yashdani55 <yashdani55@gmail.com>
2021-07-19 13:30:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Userform from './components/Userform';
+
+function App() {
+  return (
+    <div>
+      <Userform></Userform>
+    </div>
+  );
+}
+
+export default App;
