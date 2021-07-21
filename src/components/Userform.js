b1b89a13498bd2bc2fbcaf3b4c6efc0cc437f24f
yashdani55 <yashdani55@gmail.com>
2021-07-21 15:16:50 +0530

    dynamic options added in drop box

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,10 +1,16 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Messages from "./Messages";
 export default function UserForm() {
 
   const [userform, setUserForm] = useState({ firstname: "Ram", age: 20 });
-  const [message,setMessage]=useState({type:"",text:""});
+  const [message, setMessage] = useState({ type: "", text: "" });
+  const [skills, setSkills] = useState([]);
+  useEffect(() => {
+    const promise = axios.get("http://localhost:4200/skills");
+    promise.then((response) => setSkills(response.data));
+  })
+
   const handleEvent = function (event) {
     setUserForm({ ...userform, [event.target.name]: event.target.value })
   }
@@ -15,12 +21,12 @@ export default function UserForm() {
     const promise = axios.post("http://localhost:4200/users", userform);
     promise.then(function (response) {
       console.log(response);
-      setMessage({...message, type: 'success', text: "Record was saved"});
+      setMessage({ ...message, type: 'success', text: "Record was saved" });
     })
-    promise.catch(function(error){
-      setMessage({...message, type: 'error', text: "Record was not saved"})
+    promise.catch(function (error) {
+      setMessage({ ...message, type: 'error', text: "Record was not saved" })
     })
-  
+
   }
   function handleSelectChange(event) {
     console.log(event.target.value);
@@ -32,24 +38,32 @@ export default function UserForm() {
     <div>
       <h3>Create User</h3>
       <Messages message={message}></Messages>
+
       <div className="form-group">
         <label htmlFor="fname">First Name</label>
         <input name='firstname' placeholder="First Name" id="fname" className="form-control" value={userform.firstname} onChange={handleEvent}></input>
       </div>
+
       <label htmlFor="age">Age</label>
       <div className="form-group">
         <input name='age' type='number' className="form-control" id="age" placeholder="Age" value={userform.age} onChange={handleEvent}></input>
       </div>
+
       <div className="form-group">
         <label htmlFor="doj">Joining Date</label>
         <input type="date" name="date" id="doj" className="form-control" onChange={handleEvent}></input>
       </div>
+
       <div className="dropdown">
       <select onChange={handleSelectChange} name="skill">
-        <option value="default" defaultValue>Select the Skill</option>
-        <option value="HTML">HTML</option>
-        <option value="CSS">CSS</option>
-        <option value="JavaScript">JavaScript</option>
+      <option selected>Select the category</option>
+        {
+          
+          skills.map((skill) => {
+            return <option value={skill}>{skill}</option>
+          })
+        }
+        
       </select>
       </div>
       <button className="btn btn-primary" onClick={save}>Save</button>
--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,40 +1,40 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 import Counter from "./Counter";
-export default function Userlist(){
-    const [users,setUsers]= useState([]);
-    const [flags,setFlags]= useState([]);
-    useEffect(()=>{
-        if(users.length!==0){
+export default function Userlist() {
+    const [users, setUsers] = useState([]);
+    const [flags, setFlags] = useState([]);
+    useEffect(() => {
+        if (users.length !== 0) {
             return;
         }
-        console.log("called...")
-        const promise=axios.get("http://localhost:4200/users");
-        promise.then((response)=>setUsers(response.data))
+        console.log("called...");
+        const promise = axios.get("http://localhost:4200/users");
+        promise.then((response) => setUsers(response.data))
     })
-    const deleteUser=function (){
-        const promise=axios.delete("http://localhost:4200/users/"+this);
-        promise.then(response=>{
-            users.splice(arguments[0],1);
-            const users1 =[...users];
+    const deleteUser = function () {
+
+
+        const promise = axios.delete("http://localhost:4200/users/" + this);
+        promise.then(response => {
+            users.splice(arguments[0], 1);
+            const users1 = [...users];
             setUsers(users1);
         })
-      }
-      const sortByAge=()=>{
-          setFlags(!setFlags);
-          users.sort(function(a,b)
-          {
-            if (flags)
-            {
-                return a.age-b.age;
-            }  
-            else{
-                return b.age-a.age;
+    }
+    const sortByAge = () => {
+        setFlags(!setFlags);
+        users.sort(function (a, b) {
+            if (flags) {
+                return a.age - b.age;
             }
-            
-          });
-          setUsers([...users]);
-      }
+            else {
+                return b.age - a.age;
+            }
+
+        });
+        setUsers([...users]);
+    }
 
     return (
         <div>
@@ -42,24 +42,24 @@ export default function Userlist(){
             <table className="table table-border table-striped table-hover">
                 <thead>
                     <tr>
-                    <th>First Name</th>
-                    <th onClick={sortByAge}>Age</th>
-                    <th>Joining Date</th>
-                    <th>Skill</th>
+                        <th>First Name</th>
+                        <th onClick={sortByAge}>Age</th>
+                        <th>Joining Date</th>
+                        <th>Skill</th>
                     </tr>
-                    </thead>
+                </thead>
                 <tbody>
-                    {users.map((user,index)=><tr key={user.id}>
+                    {users.map((user, index) => <tr key={user.id}>
                         <td>{user.firstname}</td>
                         <td>{user.age}</td>
                         <td>{user.date}</td>
                         <td>{user.skill}</td>
                         <td><button className="btn btn-danger" onClick={deleteUser.bind(user.id)}>Delete</button></td>
-                        </tr>
+                    </tr>
                     )
                     }
                 </tbody>
-                
+
             </table>
 
         </div>
